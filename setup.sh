7ec0a2a01d1a504f711ccf46b2573603c5cfb64c
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2022-03-27 12:36:37 -0400

    updated setup to autogenerate passwd

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 MODE="${1:-simple}"
-
+USER="${2:-repouser}"
+PASS="${3:-repopass}"
 echo $MODE
 pushd $MODE
-# pwd
 [ ! -d data ] && mkdir data
+[ $MODE == "secure" ] && htpasswd -bcB htpasswd $USER $PASS
 docker-compose up -d
 popd
