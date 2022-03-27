7553a32d5ebb443338d884cc2fb5359994434bc6
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2022-03-27 13:20:33 -0400

    updated setup for better interface

--- setup.sh
+++ setup.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 
-MODE="${1:-simple}"
-USER="${2:-repouser}"
-PASS="${3:-repopass}"
-echo $MODE
+MODE=simple
+USER=repouser
+PASS=repopass
+
+usage()
+{
+cat << EOF
+usage: ./setup.sh (-param=value)
+-m, --mode (default: simple) (options: simple,secure)
+-u, --user (default: repouser) only needed for secure mode
+-p, --pass (default: repopass) only needed for secure mode
+-h, --help
+EOF
+}
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -m | --mode )
+            MODE=$VALUE
+        ;;
+        -u | --user )
+            USER=$VALUE
+        ;;
+        -p | --pass )
+            PASS=$VALUE
+        ;;
+        -h | --help )    usage
+            exit
+    esac
+    shift
+done
+
 pushd $MODE
 [ ! -d data ] && mkdir data
 [ $MODE == "secure" ] && htpasswd -bcB htpasswd $USER $PASS
